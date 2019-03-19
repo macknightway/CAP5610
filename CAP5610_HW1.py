517120fd851c075368400ab0fd1fde262f1ce1fe
Justin MacKenzie <justinmackenzie@Justins-MacBook-Pro.local>
2019-03-18 20:51:44 -0400

    CAP5610_HW_1: Adding solution for problem 3
    Now supports logistic regression with categorical-cross entropy
    loss function and softmax activation.

--- CAP5610_HW1.py
+++ CAP5610_HW1.py
@@ -1,13 +1,18 @@
 from keras.datasets import mnist
+from keras.utils import to_categorical
 import math
 import numpy as np
+import sys
 
 def sigmoid(z):
-  return 1 / (1 + math.exp(-z))
+    return 1 / (1 + math.exp(-z))
 
 
-def softmax(z):
-    pass
+def softmax(z, z_list):
+    z_vec = np.array(z_list)
+    denom = np.sum(np.exp(z_vec))
+    num = np.exp(z)
+    return np.divide(num,denom)
 
 
 def sgd_mse(a, y, x, w, b, lr):
@@ -27,10 +32,74 @@ def sgd_bce(a, y, x, w, b, lr):
     return w_update, b_update
 
 
-def sgd_cce(a, y, x, w, b, lr):
-    pass
+def sgd_cce(a, y, x, w, b, lr, j):
+    gradient = 0
+    w_gradient = 0
+    b_gradient = 0
+    for k in range(10):
+        if k == j:
+            k_delta = 1
+        else:
+            k_delta = 0
+        gradient = gradient + (y[k] * (a - k_delta))
+    w_gradient = gradient * x
+    b_gradient = gradient
+    w_update = w - lr * w_gradient
+    b_update = b - lr * b_gradient
+    return w_update, b_update
 
-if __name__ == '__main__':
+
+def logistic_regression_softmax():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    y_train = to_categorical(y_train)
+
+    x_train = x_train.reshape((60000, 28 * 28))
+    x_train = x_train.astype('float32') / 255
+
+    #x_test = x_test.reshape((10000, 28 * 28))
+    #x_test = x_test.astype('float32') / 255
+
+    w = [[0] * 784, [0] * 784,[0] * 784,[0] * 784,[0] * 784,
+         [0] * 784, [0] * 784,[0] * 784,[0] * 784,[0] * 784]
+    b = [0] * 10
+
+    lr = .001
+
+    for epoch in range(10):
+        # Train with batch size of 1
+        for i in range(y_train.shape[0]):
+            z = []
+            a = []
+            x = x_train[i]
+            for neuron in range(10):
+                z.append(np.dot(x, w[neuron]))
+            for neuron in range(10):
+                a.append(softmax(z[neuron], z))
+
+            if i == 45:
+                print(a)
+
+            y_hat = np.argmax(a)
+            y_arg_max = np.argmax(y_train[i])
+
+            if y_hat == y_arg_max:
+                is_digit_prediction = True
+            else:
+                is_digit_prediction = False
+
+            for j in range(10):
+                w[j], b[j] = sgd_cce(a[j], y_train[i], x, w[j], b[j], lr, j)
+
+            if i % 1000 == 0:
+                print("Y_Hat: {0}".format(y_hat))
+                print("Y: {0}".format(y_arg_max))
+                print(a)
+                print("")
+
+
+
+def logistic_regression_sigmoid():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     y_train_mat = [[], [], [], [], [], [], [], [], [], []]
@@ -47,8 +116,8 @@ if __name__ == '__main__':
     x_train = x_train.reshape((60000, 28 * 28))
     x_train = x_train.astype('float32') / 255
 
-    x_test = x_test.reshape((10000, 28 * 28))
-    x_test = x_test.astype('float32') / 255
+    #x_test = x_test.reshape((10000, 28 * 28))
+    #x_test = x_test.astype('float32') / 255
 
     w = [0] * 784
     b = [0]
@@ -59,11 +128,11 @@ if __name__ == '__main__':
 
     classifiers = [0] * 10
 
-    #Train k classifiers
+    # Train k classifiers
     for k in range(10):
-        #Train for 100 epoch
+        # Train for 100 epoch
         for epoch in range(10):
-            #Train with batch size of 1
+            # Train with batch size of 1
             for i in range(y_train_mat[k].shape[0]):
                 x = x_train[i]
                 z = np.dot(x, w) + b
@@ -79,9 +148,13 @@ if __name__ == '__main__':
 
                 if epoch > 90:
                     if (y and is_digit_prediction) or (not y and not is_digit_prediction):
-                        #Correct prediction
+                        # Correct prediction
                         correct += 1
                     else:
-                        #Incorrect prediction
+                        # Incorrect prediction
                         incorrect += 1
-        classifiers[k] = (w,b)
+        classifiers[k] = (w, b)
+
+if __name__ == '__main__':
+    #logistic_regression_sigmoid()
+    logistic_regression_softmax()
